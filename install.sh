fcd719e930b453c6122c6906618710656af574ea
Hendrik Mans <hendrik@mans.de>
2011-12-17 17:47:32 +0100

    Add some sanity to the install script.

--- install.sh
+++ install.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+NOW=$(date +%s)
+
+run () {
+  echo $@
+  $@
+}
+
+install () {
+  SRC="$PWD/$1"
+  DEST="$HOME/$1"
+
+  if [ -h "$DEST" ]; then      # if $DEST is a symlink, we can probably delete it.
+    run rm "$DEST"
+  elif [ -e "$DEST" ]; then
+    run mv "$DEST" "$DEST.orig-$NOW"
+  fi
+  run ln -s "$SRC" "$DEST"
+}
+
 for F in bin .inputrc .irbrc .bash_profile .gitconfig ; do
-  SRC="${PWD}/${F}"
-  DEST="${HOME}/${F}"
-  echo mv "${DEST}" "${DEST}.orig"
-  mv "${DEST}" "${DEST}.orig"
-  echo ln -s "${SRC}" "${DEST}"
-  ln -s "${SRC}" "${DEST}"
+  install ${F}
 done
