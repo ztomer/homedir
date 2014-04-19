a74cde4329e0728a60d88b5de63a739f6d1014df
ztomer <ztomer@gmail.com>
2014-04-19 20:32:13 +0300

    multi monitor now works
    fixed multiple monitor support, simplified code

--- .phoenix.js
+++ .phoenix.js
@@ -7,25 +7,6 @@
 
 var BORDER_OFFSET = 20;
 
-function frameToRect(frame) {
-  return {
-    left: frame.x,
-    right: frame.x + frame.width,
-    top: frame.y,
-    bottom: frame.y + frame.height
-  }
-}
- 
-// {top, bottom, left right} => {x, y, height, width}
-function rectToFrame(rect) {
-  return {
-    x: rect.left,
-    y: rect.top,
-    width: rect.right - rect.left,
-    height: rect.bottom - rect.top
-  }
-}
- 
 // Cycle args for the function, if called repeatedly
 // cycleCalls(fn, [ [args1...], [args2...], ... ])
 var lastCall = null
@@ -44,136 +25,123 @@ function cycleCalls(fn, argsList) {
 // toAnything(a,b). Sets the focusWindow size to screensize * a/b
 function toLeft(x_ratio) {
   var win = Window.focusedWindow()
-  var rect = frameToRect(win.frame())
-  var screenFrame = win.screen().frameWithoutDockOrMenu()
+  var screenFrame = win.screen().frameWithoutDockOrMenu();
+  var winFrame = win.frame();
 
-  rect.left = 0
-  rect.right = screenFrame.width * x_ratio
-  rect.top = 0 + BORDER_OFFSET
-  rect.bottom = screenFrame.height + BORDER_OFFSET
-
-  win.setFrame(rectToFrame(rect), win.screen())
+  winFrame.x = screenFrame.x
+  winFrame.y = screenFrame.y
+  winFrame.width = screenFrame.width * x_ratio
+  winFrame.height = screenFrame.height
+  
+  win.setFrame(winFrame)
 }
 
 function toTopLeft(x_ratio) {
-  // Done
   var win = Window.focusedWindow()
+  var screenFrame = win.screen().frameWithoutDockOrMenu();
+  var winFrame = win.frame();
+
+  winFrame.x = screenFrame.x
+  winFrame.y = screenFrame.y
+  winFrame.width = screenFrame.width * x_ratio
+  winFrame.height = screenFrame.height * 0.5
   
-  var winFrame = win.frame()
-  var screenFrame = win.screen().frameWithoutDockOrMenu()
-
-  var rect = frameToRect(winFrame)
-  rect.left = 0
-  rect.right = screenFrame.width * x_ratio
-
-  rect.top = 0 + BORDER_OFFSET
-  rect.bottom = screenFrame.height * 0.5 + BORDER_OFFSET
-  
-  winFrame = rectToFrame(rect)
-
-  win.setFrame(winFrame, win.screen())
+  win.setFrame(winFrame)
 }
 
 function toBottomLeft(x_ratio) {
-  // DONE
   var win = Window.focusedWindow()
-  var winFrame = win.frame()
+  var screenFrame = win.screen().frameWithoutDockOrMenu();
+  var winFrame = win.frame();
 
-  var screenFrame = win.screen().frameWithoutDockOrMenu()
+  winFrame.x = screenFrame.x
+  winFrame.y = screenFrame.y + screenFrame.height * 0.5
+  winFrame.width = screenFrame.width * x_ratio
+  winFrame.height = screenFrame.height * 0.5
   
-  var rect = frameToRect(winFrame)
-  rect.left = 0
-  rect.right = screenFrame.width * x_ratio
-
-  rect.top = screenFrame.height * 0.5 + BORDER_OFFSET
-  rect.bottom = screenFrame.height  + BORDER_OFFSET  
-
-  win.setFrame(rectToFrame(rect) , win.screen())
+  win.setFrame(winFrame)
 }
  
 function toRight(x_ratio) {
   var win = Window.focusedWindow()
-  var rect = frameToRect(win.frame())
   var screenFrame = win.screen().frameWithoutDockOrMenu()
-  rect.left = screenFrame.width - screenFrame.width * x_ratio
-  rect.right = screenFrame.width
-  rect.top = 0 + BORDER_OFFSET
-  rect.bottom = screenFrame.height + BORDER_OFFSET
-  win.setFrame(rectToFrame(rect), win.screen())
+  var winFrame = win.frame()
+
+  winFrame.x = screenFrame.x + screenFrame.width * (1 - x_ratio)
+  winFrame.y = screenFrame.y
+  winFrame.width = screenFrame.width * x_ratio
+  winFrame.height = screenFrame.height
+  
+  win.setFrame(winFrame)
+
 }
 
 function toTopRight(x_ratio) {
-  // DONE
   var win = Window.focusedWindow()
+  var screenFrame = win.screen().frameWithoutDockOrMenu()
   var winFrame = win.frame()
-  var screenFrame = win.screen().frameWithoutDockOrMenu()
 
-  var rect = frameToRect(winFrame)
-  var screenFrame = win.screen().frameWithoutDockOrMenu()
-  rect.left = screenFrame.width - screenFrame.width * x_ratio
-  rect.right = screenFrame.width
-
-  rect.top = 0 + BORDER_OFFSET
-  rect.bottom = screenFrame.height * 0.5 + BORDER_OFFSET
-
-  winFrame = rectToFrame(rect)
-
-  win.setFrame(winFrame, win.screen())
+  winFrame.x = screenFrame.x + screenFrame.width * (1 - x_ratio)
+  winFrame.y = screenFrame.y
+  winFrame.width = screenFrame.width * x_ratio
+  winFrame.height = screenFrame.height * 0.5
+  
+  win.setFrame(winFrame)
 }
 
 function toBottomRight(x_ratio) {
-  // DONE
   var win = Window.focusedWindow()
-  var winFrame = win.frame()
-  
-  var rect = frameToRect(winFrame)
   var screenFrame = win.screen().frameWithoutDockOrMenu()
-  rect.left = screenFrame.width * (1 - x_ratio)
-  rect.right = screenFrame.width 
-  rect.top = screenFrame.height * 0.5 + BORDER_OFFSET
-  rect.bottom = screenFrame.height + BORDER_OFFSET
- 
-  win.setFrame(rectToFrame(rect), win.screen())
+  var winFrame = win.frame();
+
+  winFrame.x = screenFrame.x + screenFrame.width * (1 - x_ratio)
+  winFrame.y = screenFrame.y + screenFrame.height * 0.5
+  winFrame.width = screenFrame.width * x_ratio
+  winFrame.height = screenFrame.height * 0.5
+  
+  win.setFrame(winFrame)
+
 }
  
+ 
 function toTop(x_ratio) {
-  //DONE 
   var win = Window.focusedWindow()
-  var rect = frameToRect(win.frame())
+  var winFrame = win.frame()
   var screenFrame = win.screen().frameWithoutDockOrMenu()
-  rect.left = screenFrame.width * (1 - x_ratio)
-  rect.right = screenFrame.width * (x_ratio)
+
+  winFrame.x = screenFrame.x + screenFrame.width * (1 - x_ratio) * 0.5
+  winFrame.y = screenFrame.y 
+  winFrame.height = screenFrame.height * 0.5
+  winFrame.width = screenFrame.width * x_ratio
   
-  rect.top = + BORDER_OFFSET
-  rect.bottom = screenFrame.height * 0.5 + BORDER_OFFSET
-  win.setFrame(rectToFrame(rect), win.screen())
+  win.setFrame(winFrame)
 }
  
 function toBottom(x_ratio) {
-  // DONE 
+
   var win = Window.focusedWindow()
-  var rect = frameToRect(win.frame())
+  var winFrame = win.frame()
   var screenFrame = win.screen().frameWithoutDockOrMenu()
-  rect.left = screenFrame.width * (1 - x_ratio)
-  rect.right = screenFrame.width * (x_ratio)
+
+  winFrame.x = screenFrame.x + screenFrame.width * (1 - x_ratio) * 0.5
+  winFrame.y = screenFrame.y + screenFrame.height * 0.5
+  winFrame.height = screenFrame.height * 0.5
+  winFrame.width = screenFrame.width * x_ratio
   
-  rect.top = screenFrame.height * 0.5 + BORDER_OFFSET
-  rect.bottom = screenFrame.height + BORDER_OFFSET
-  
-  win.setFrame(rectToFrame(rect), win.screen())
+  win.setFrame(winFrame)
 }
 
 function toMiddle(x_ratio) {
-  // DONE 
   var win = Window.focusedWindow()
-  var rect = frameToRect(win.frame())
+  var winFrame = win.frame()
   var screenFrame = win.screen().frameWithoutDockOrMenu()
-  rect.left = screenFrame.width * (1 - x_ratio)
-  rect.right = screenFrame.width * (x_ratio)
+
+  winFrame.x = screenFrame.x + screenFrame.width * (1 - x_ratio) * 0.5
+  winFrame.y = screenFrame.y
+  winFrame.height = screenFrame.height
+  winFrame.width = screenFrame.width * x_ratio
   
-  rect.top = 0
-  rect.bottom = screenFrame.height
-  win.setFrame(rectToFrame(rect), win.screen())
+  win.setFrame(winFrame)
 }
 
  
@@ -236,8 +204,8 @@ api.bind(',', opts, cycleCalls(
 api.bind('u', opts, cycleCalls(
   toTop,
   [
-    [0.666667],
-    [1-0.166667],
+    [0.333337],
+    [1 - 0.333337],
     [1]
   ]
 ))
@@ -245,8 +213,8 @@ api.bind('u', opts, cycleCalls(
 api.bind('m', opts, cycleCalls(
   toBottom,
   [
-    [0.666667],
-    [1-0.166667],
+    [0.333337],
+    [1 - 0.333337],
     [1]
   ]
 ))
@@ -254,8 +222,8 @@ api.bind('m', opts, cycleCalls(
 api.bind('j', opts, cycleCalls(
   toMiddle,
   [
-    [0.666667],
-    [1-0.166667],
+    [0.333337],
+    [1 - 0.333337],
     [1]
   ]
 ))
